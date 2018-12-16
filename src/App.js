b326d39cf83baa968bfb133435b250679bd6504d
WanguM <susanwangu@gmail.com>
2018-12-16 05:12:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [],
+    todo: ""
+  };
+
+  saveToState = e => {
+    //set the state of todo on every input change and set default value of done: false
+    this.setState({
+      todo: {
+        task: e.target.value,
+        done: false
+      }
+    });
+  };
+
+  // when a todo is complete
+  onComplete = _todo => {
+    // 1. fetch all todos
+    let todos = this.state.todos;
+    // 2. look for the specific todo
+    todos.filter(todo => {
+      if (todo.task.includes(_todo)) {
+        // 3. change done from false to true
+        todo.done = !todo.done;
+        return;
+      }
+    });
+
+    // update state of todos
+    this.setState({
+      todos
+    });
+  };
+
   render() {
+    const { todo, todos } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <h2>Add Todo</h2>
+        <div className="todo-form">
+          <form
+            method="post"
+            onSubmit={async e => {
+              e.preventDefault();
+              todos.push(todo);
+              this.setState({ todo: { task: "", done: false } });
+            }}
           >
-            Learn React
-          </a>
-        </header>
+            <fieldset>
+              <label htmlFor="todo">
+                Todo
+                <input
+                  type="text"
+                  name="todo"
+                  placeholder="Enter Todo"
+                  value={todo.task}
+                  onChange={this.saveToState}
+                />
+              </label>
+
+              <button type="submit">Add Todo</button>
+            </fieldset>
+          </form>
+        </div>
+        <div>
+          <h2>Wangu's Todos</h2>
+          <div>
+            <ul>
+              {todos.map(_todo => (
+                <li className="todoTask">
+                  <label className="task-label">
+                    <input
+                      type="checkbox"
+                      className="form-check"
+                      onChange={() => this.onComplete(_todo.task)}
+                    />
+                    {/* check if the task is done or not, if done strike through */}
+                    {_todo.done ? (
+                      <strike>{_todo.task}</strike>
+                    ) : (
+                      <p>{_todo.task}</p>
+                    )}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
